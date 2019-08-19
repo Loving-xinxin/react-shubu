9da5c125a9c3717b3eabe05b0040b27d54320937
Loving-xinxin <2603712235@qq.com>
2019-08-19 15:16:34 +0800

    first update

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class About extends Component {
+  state = {};
+  render() {
+    return <div className="about">about</div>;
+  }
+}
+
+export default About;

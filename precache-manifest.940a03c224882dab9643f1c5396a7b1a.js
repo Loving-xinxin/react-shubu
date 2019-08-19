2d7853e5d6c97748cdf8126923e5a8b6a7db80d5
Loving-xinxin <2603712235@qq.com>
2019-08-19 15:45:07 +0800

    two update

---
+++ precache-manifest.940a03c224882dab9643f1c5396a7b1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edde03de378543e34bed7f2ba9e66f99",
+    "url": "/react-shubu/index.html"
+  },
+  {
+    "revision": "db8f3341c8852e570145",
+    "url": "/react-shubu/static/css/main.2c6d4145.chunk.css"
+  },
+  {
+    "revision": "0681e5bb692bcc9416aa",
+    "url": "/react-shubu/static/js/2.ed64165c.chunk.js"
+  },
+  {
+    "revision": "db8f3341c8852e570145",
+    "url": "/react-shubu/static/js/main.8ed642f6.chunk.js"
+  },
+  {
+    "revision": "52aea03c93231496526a",
+    "url": "/react-shubu/static/js/runtime~main.c72a56f8.js"
+  }
+]);
